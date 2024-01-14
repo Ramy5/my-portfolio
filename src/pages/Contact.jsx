cf5641862ceacc2a9fa10ae6152d67e31af1c974
Ramy5 <ramyjounior464@gmail.com>
2024-01-14 00:50:36 -0800

    access environment

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -18,6 +18,10 @@ const Contact = () => {
     e.preventDefault();
     setIsLoading(true);
 
+    console.log(import.meta.env.VITE_APP_EMAILJS_SERVICE_ID)
+    console.log(import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID)
+    console.log(import.meta.env.VITE_APP_EMAILJS_KEY)
+
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
